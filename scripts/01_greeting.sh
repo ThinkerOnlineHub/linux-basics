24b266d0ece49cb7cd2b75bc58764f43578ae901
Thinker Online Hub <98183786+ThinkerOnlineHub@users.noreply.github.com>
2026-04-20 23:07:45 +0530

    Create 01_greeting.sh

---
+++ scripts/01_greeting.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Greeting Script
+
+GREETING="Welcome to DevOps Learning"
+
+echo "Hello $USER, $GREETING"
+
+echo "Creating a backup folder..."
+mkdir -p "$HOME/my_backup"
+
+echo "Task Completed!"
