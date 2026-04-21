c7c183d46966869898418a33219e27a985a9e1fc
Thinker Online Hub <98183786+ThinkerOnlineHub@users.noreply.github.com>
2026-04-21 22:32:32 +0530

    Create marathi_05_simple_loop.sh

---
+++ मराठी_Marathi/scripts_marathi/marathi_05_simple_loop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Loop mhanje ekach kaam tharaavik veles (repeatedly) karne.
+# Real-life Example: Tula 1 te 5 numbers print karayche ahet.
+
+# Simple For Loop
+for i in {1..5}
+do
+   echo "Number: $i"
+done
