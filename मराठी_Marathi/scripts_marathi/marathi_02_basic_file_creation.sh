e0e019076f58dc8639e23c396598c1f7d225ad7b
Thinker Online Hub <98183786+ThinkerOnlineHub@users.noreply.github.com>
2026-04-21 22:26:29 +0530

    Create marathi_02_basic_file_creation.sh

---
+++ मराठी_Marathi/scripts_marathi/marathi_02_basic_file_creation.sh
@@ -0,0 +1,17 @@
+# Ha mi code lihila hota. login tar yogya ahe pan DevOps Pro
+# pramane to pudhe dilela ahe tasa code lihinyacha try kara.
+
+# -----------------------------shell script -----------------------------
+
+#!/bin/bash
+
+# 1. create directory | directory/folder banavnyasathichi command
+mkdir ~/1st-script
+
+# 2. make file | file banavnyasathichi command
+touch ~/1st-script/1st-file.txt
+
+# 3. write text on file | file madhe text add karnyasathichi command
+echo "Success" > ~/1st-script/1st-file.txt
+cat ~/1st-script/1st-file.txt
+echo "Done!"
