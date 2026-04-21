4f0c66aaa1d566546fad082d8ff2fc108e50bf0d
Thinker Online Hub <98183786+ThinkerOnlineHub@users.noreply.github.com>
2026-04-21 22:35:18 +0530

    Create marathi_06_multiple_folder_creation.sh

---
+++ मराठी_Marathi/scripts_marathi/marathi_06_multiple_folder_creation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Jar tula एकाच वेळी 3 folders (Month_1, Month_2, Month_3) banvayche astil, tar loop kasa vapraycha?
+# for loop use kartana
+# multiple folder 1kach vel create karnyasathichi script
+
+for folder in Month_1 Month_2 Month_3
+do
+	echo "Creating Folder: $folder"
+	mkdir $folder
+done
+echo "sagle banun tayar ahet!"
