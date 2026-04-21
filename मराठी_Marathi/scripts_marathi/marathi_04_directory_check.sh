2ca6f9d44cbb71b910f3ddb195962d4bc69d9dbc
Thinker Online Hub <98183786+ThinkerOnlineHub@users.noreply.github.com>
2026-04-21 22:49:17 +0530

    Update marathi_04_directory_check.sh

--- मराठी_Marathi/scripts_marathi/marathi_04_directory_check.sh
+++ मराठी_Marathi/scripts_marathi/marathi_04_directory_check.sh
@@ -1,4 +1,10 @@
+# Task: Linux madhe script "Hushar" banvnya-sathi Conditions garjechya ahet.
+# **Scenario:** Jar folder aadhi pasunach asel, tar script ne "Folder already exists" mhanle pahije, ani nasel tar banvle pahije.
+# -----------------------------shell script -----------------------------
+
 #directory check karnyasathi ani jar folder/directory available nasel tar navin banavnyasathi chi script
+
+#!/bin/bash
 
 if [ -d ~/bash-scripts/2nd-script ]; then
         echo "Directory Exists."
