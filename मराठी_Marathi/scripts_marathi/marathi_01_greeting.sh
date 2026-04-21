fddeba8a19d1a722b623226bcf2b161ca16a77b9
Thinker Online Hub <98183786+ThinkerOnlineHub@users.noreply.github.com>
2026-04-21 22:43:37 +0530

    Update marathi_01_greeting.sh

--- मराठी_Marathi/scripts_marathi/marathi_01_greeting.sh
+++ मराठी_Marathi/scripts_marathi/marathi_01_greeting.sh
@@ -1,3 +1,11 @@
+# Task: Ek script banav ji Greetings Text Print karel mhanje script run keli tar tumhi jo msg lihila ahe to dakhavel.
+# Linux madhe file banvlya-banvlya ti "Executable" naste. Tula tila power dyavi lagel:
+# commmand: `chmod +x myscript.sh`
+
+# Run Karaych asel tar pudhil command use kara
+# command: `./myscript.sh`
+# -----------------------------shell script -----------------------------
+
 #!/bin/bash
 
 # 1. Variable define kara
@@ -12,8 +20,4 @@ mkdir -p ~/my_backup
 echo "Done!"
 
 
-# Linux madhe file banvlya-banvlya ti "Executable" naste. Tula tila power dyavi lagel:
-# commmand: `chmod +x myscript.sh`
 
-# Run Karaych asel tar pudhil command use kara
-# command: `./myscript.sh`
