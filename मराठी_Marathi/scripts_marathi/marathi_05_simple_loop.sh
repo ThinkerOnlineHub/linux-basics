fd36988aaa4b07dcd9197826c8718c98b742a647
Thinker Online Hub <98183786+ThinkerOnlineHub@users.noreply.github.com>
2026-04-21 22:50:43 +0530

    Update marathi_05_simple_loop.sh

--- मराठी_Marathi/scripts_marathi/marathi_05_simple_loop.sh
+++ मराठी_Marathi/scripts_marathi/marathi_05_simple_loop.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
-
+# Task: Loop mhanje ekach kaam tharaavik veles (repeatedly) karne.
+# **Real-life Example:** Tula 1 te 5 numbers print karayche ahet.
 # Loop mhanje ekach kaam tharaavik veles (repeatedly) karne.
 # Real-life Example: Tula 1 te 5 numbers print karayche ahet.
+
+# -----------------------------shell script -----------------------------
+
+#!/bin/bash
+
 
 # Simple For Loop
 for i in {1..5}
