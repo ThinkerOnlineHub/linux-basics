fe09231d3f613f3c5d09dbdb8166c576340d8319
Thinker Online Hub <98183786+ThinkerOnlineHub@users.noreply.github.com>
2026-04-21 22:30:41 +0530

    Create marathi_04_directory_check.sh

---
+++ मराठी_Marathi/scripts_marathi/marathi_04_directory_check.sh
@@ -0,0 +1,7 @@
+#directory check karnyasathi ani jar folder/directory available nasel tar navin banavnyasathi chi script
+
+if [ -d ~/bash-scripts/2nd-script ]; then
+        echo "Directory Exists."
+else
+        mkdir ~/bash-scripts/2nd-script
+fi
